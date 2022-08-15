38f6d54351abfbe42a5bfaa067359caf65f77d67
Surya <suryaprakashsharma453@gmail.com>
2022-08-16 01:12:59 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, } from 'react';
 import './App.css';
 
+
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, getData] = useState([])
+    const URL = 'https://opensheet.elk.sh/1gH5Kle-styszcHF2G0H8l1w1nDt1RhO9NHNCpHhKK0M/employees';
+ 
+    useEffect(() => {
+        fetchData()
+    }, [])
+ 
+ 
+    const fetchData = () => {
+        fetch(URL)
+            .then((res) =>
+                res.json())
+ 
+            .then((response) => {
+                console.log(response);
+                getData(response);
+            })
+ 
+    }
+ 
+    return (
+        <>
+            <h1>Employees details list</h1>
+            <tbody>
+                <tr>
+                    <th>Id</th>
+                    <th>First_Name</th>
+                    <th>Last_Name</th>
+                    <th>Date_of_Birth</th>
+                    <th>Address</th>
+                    <th>Date_of_Joining</th>
+                    <th>Salary</th>
+                    <th>Designation</th>
+                </tr>
+                {data.map((item, i) => (
+                    <tr key={i}>
+                        <td><a href="/https://opensheet.elk.sh/1gH5Kle-styszcHF2G0H8l1w1nDt1RhO9NHNCpHhKK0M/Alex">{item.id}</a></td>
+                        <td>{item.first_name}</td>
+                        <td>{item.last_name}</td>
+                        <td>{item.date_of_birth}</td>
+                        <td>{item.address}</td>
+                        <td>{item.date_of_joining}</td>
+                        <td>{item.salary}</td>
+                        <td>{item.designation}</td>
+                    </tr>
+                ))}
+                
+            </tbody>
+            
+            
+        </>
+        
+    );
 }
 
 export default App;
